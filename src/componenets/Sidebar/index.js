712b49c00b5b3bf627c570e6b3cd2c91c49f10c4
xinnan cao <xinnancao@gmail.com>
2023-02-04 14:35:53 -0500

    added sidebars

--- src/componenets/Sidebar/index.js
+++ src/componenets/Sidebar/index.js
@@ -1,15 +1,51 @@
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import './index.scss';
 import LogoS from '../../assets/images/logo-s.png'; 
 import LogoSubtitle from '../../assets/images/logo_sub.png'; 
-const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt="logo"/>
-            <img src={LogoSubtitle} alt="slobodan"/>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope,faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faInstagram, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
-       
+const Sidebar = () => (
+    <div className="nav-bar">
+        <Link className="logo" to="/">
+            <img src={LogoS} alt="logo"/>
+            <img className="sub-logo" src={LogoSubtitle} alt="slobodan"/>
         </Link>
+        <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+        </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/xinnancao/">
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href="https://github.com/xinnnan">
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href="https://www.instagram.com/tufilmlife/">
+                    <FontAwesomeIcon icon={faInstagram} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href="https://www.youtube.com/@user-fm7ew7sc9t">
+                    <FontAwesomeIcon icon={faYoutube} color="#4d4d4e"/>
+                </a>
+            </li>
+        </ul>
+    
     </div>
 )
 
